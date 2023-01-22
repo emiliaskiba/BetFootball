b37649b6f3f4516ee66dd267f00adf2d88aca02b
emiliaskiba <kapitanema@gmail.com>
2023-01-22 23:25:25 +0100

    New functionalities: Teams - displaying team members and scores! (DONE)

--- app/src/main/java/com/pollub/betfootball/Activity/AdminFillInfoMatch.java
+++ app/src/main/java/com/pollub/betfootball/Activity/AdminFillInfoMatch.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.pollub.betfootball.Entity.Bet;
 import com.pollub.betfootball.Entity.Match;
+import com.pollub.betfootball.Entity.User;
 import com.pollub.betfootball.R;
 
 import java.util.Objects;
@@ -29,10 +30,11 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
     private ImageView back;
     private EditText teamOneScoreEdit, teamTwoScoreEdit;
     private Button addAndCalculate;
-    private DatabaseReference reference, referenceTeam1, referenceTeam2, referenceBets, referenceChosenMatch;
-    private String value;
+    private DatabaseReference reference, referenceTeam1, referenceTeam2, referenceBets, referenceChosenMatch, referenceUser;
+    private String value, userID;
     private Integer teamOneScore;
     private Integer teamTwoScore;
+    private Integer tempScore, tempScoreMatchday, tempScoreAllSeason;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,7 +129,6 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
     private void calculate(String matchID, Integer teamOneScore, Integer teamTwoScore) {
 
 
-
         referenceBets = FirebaseDatabase.getInstance().getReference("Bets");
 
         referenceBets.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -145,9 +146,11 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
                     }
 
                     Bet bet = snapshot.getValue(Bet.class);
+
+                    System.out.println("USERID W ŚRODKU: --------" + userID);
                     if (Objects.equals((bet.matchID), matchID)) {
                         String temp = snapshot.getKey();
-                        Integer tempScore = 0;
+                        tempScore = 0;
                         referenceBets.child(temp).child("calculated").setValue(true);
 
                         if (bet.whoWins == whoWins) {
@@ -160,6 +163,49 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
                             tempScore = tempScore + 5;
                         }
                         referenceBets.child(temp).child("score").setValue(tempScore);
+
+                      //  Handler handler = new Handler();
+                      //  handler.postDelayed(new Runnable() {
+                       //     public void run() {
+                                System.out.println("Po poczekanku: --------" + userID);
+                        referenceUser = FirebaseDatabase.getInstance().getReference().child("Users");
+
+                        referenceUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                System.out.println("W datachange: --------" + userID);
+                                userID = bet.userID;
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                                    String tempUserID = snapshot.getKey();
+                                    System.out.println("Przed porównaniem: --------" + userID);
+                                    System.out.println("tempUserID: --------" + tempUserID);
+                                    if (Objects.equals(userID, tempUserID)) {
+                                        User user = snapshot.getValue(User.class);
+                                        tempScoreMatchday = user.getScoreMatchDay();
+                                        System.out.println("tempScore: --------" + tempScore);
+                                        System.out.println("tempScoreMatchday: --------" + tempScoreMatchday);
+                                        tempScoreMatchday = tempScoreMatchday + tempScore;
+                                        System.out.println("tempScoreMatchday po dodaniu: --------" + tempScoreMatchday);
+                                        tempScoreAllSeason = user.getScoreAllSeason();
+                                        System.out.println("tempScoreMatchday: --------" + tempScoreAllSeason);
+                                        tempScoreAllSeason = tempScoreAllSeason + tempScore;
+                                        System.out.println("tempScoreMatchday po dodaniu: --------" + tempScoreAllSeason);
+                                        referenceUser.child(userID).child("scoreMatchDay").setValue(tempScoreMatchday);
+                                        referenceUser.child(userID).child("scoreAllSeason").setValue(tempScoreAllSeason);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(AdminFillInfoMatch.this, "Can't load. Make sure your connection is stable", Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+                          //  }
+                       // }, 1000);
                     }
                 }
             }
@@ -173,7 +219,6 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
 
 
     }
-
 
 
     private void update(String matchID, Integer teamOneScore, Integer teamTwoScore) {
@@ -208,14 +253,11 @@ public class AdminFillInfoMatch extends AppCompatActivity implements View.OnClic
         reference.child(matchID).child("teamOneScore").setValue(teamOneScore);
         reference.child(matchID).child("teamTwoScore").setValue(teamTwoScore);
         reference.child(matchID).child("happened").setValue(true);
-        if (teamOneScore > teamTwoScore){
+        if (teamOneScore > teamTwoScore) {
             reference.child(matchID).child("whoWins").setValue(1);
-        }
-        else if (teamOneScore < teamTwoScore){
+        } else if (teamOneScore < teamTwoScore) {
             reference.child(matchID).child("whoWins").setValue(2);
         }
-
-
 
 
         Toast.makeText(AdminFillInfoMatch.this, "Match info inserted & score calculated", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/pollub/betfootball/Activity/TeamActivity.java
+++ app/src/main/java/com/pollub/betfootball/Activity/TeamActivity.java
@@ -12,25 +12,36 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.pollub.betfootball.Entity.Team;
+import com.pollub.betfootball.Entity.TeamUsers;
+import com.pollub.betfootball.Entity.User;
 import com.pollub.betfootball.R;
+
+import java.util.Objects;
 
 public class TeamActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView back;
     private String value;
-    private TextView team_name, team_code;
-    private DatabaseReference database;
-    private String teamName, teamCode;
+    private TextView team_name, team_code, leader;
+    private DatabaseReference database, referenceLeader;
+    private String teamName, teamCode, leaderID, userLeaderID, leaderName;
     private ClipboardManager myClipboard;
     private ClipData myClip;
     private Button teamCodeCopy;
 
+    private RecyclerView recyclerView;
+    TeamMembersAdapter adapter; // Create Object of the Adapter class
+    Query mbase, mbase1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,51 +52,98 @@ public class TeamActivity extends AppCompatActivity implements View.OnClickListe
 
         team_name = findViewById(R.id.team_name);
         team_code = findViewById(R.id.teamCode);
+        leader = findViewById(R.id.leader);
         teamCodeCopy = findViewById(R.id.teamCodeCopy);
 
-            database = FirebaseDatabase.getInstance().getReference().child("UserTeams").child(value);
-            database.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Team team = dataSnapshot.getValue(Team.class);
+        database = FirebaseDatabase.getInstance().getReference().child("UserTeams").child(value);
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Team team = dataSnapshot.getValue(Team.class);
 
-                            teamName = team.getName();
-                            teamCode = team.getCode();
+                teamName = team.getName();
+                teamCode = team.getCode();
+                leaderID = team.getLeader();
 
-                }
+            }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
 
-            });
+        });
 
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
 
-                    team_name.setText(teamName);
-                    team_code.setText(teamCode);
-                    myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                team_name.setText(teamName);
+                team_code.setText(teamCode);
+                myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 
-                    teamCodeCopy.setOnClickListener(new View.OnClickListener() {
+                teamCodeCopy.setOnClickListener(new View.OnClickListener() {
 
-                        @Override
-                        public void onClick(View v) {
+                    @Override
+                    public void onClick(View v) {
 
-                            myClip = ClipData.newPlainText("text", teamCode);
-                            myClipboard.setPrimaryClip(myClip);
+                        myClip = ClipData.newPlainText("text", teamCode);
+                        myClipboard.setPrimaryClip(myClip);
 
-                            Toast.makeText(TeamActivity.this, "Team Code Copied!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(TeamActivity.this, "Team Code Copied!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                referenceLeader = FirebaseDatabase.getInstance().getReference().child("Users");
+                referenceLeader.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            User user = snapshot.getValue(User.class);
+
+                            userLeaderID = snapshot.getKey();
+                             if (Objects.equals(leaderID, userLeaderID)) {
+                                leaderName = user.fullName;
+
+                            }
+
                         }
-                    });
+                    }
 
-                }
-            }, 1000);
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+
+                });
+
+                Handler handler2 = new Handler();
+                handler2.postDelayed(new Runnable() {
+                    public void run() {
+
+                        System.out.println(leaderName);leader.setText(leaderName);
+                    }
+                }, 500);
+            }
+        }, 500);
 
 
         back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(this);
+
+
+        mbase = FirebaseDatabase.getInstance().getReference().child("TeamUsers").orderByChild("teamID").equalTo(value);
+
+        recyclerView = findViewById(R.id.teamUsersList);
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<TeamUsers> options = new FirebaseRecyclerOptions.Builder<TeamUsers>().setQuery(mbase, TeamUsers.class).build();
+        // the Adapter class itself
+
+        adapter = new TeamMembersAdapter(options);
+
+        // Connecting Adapter class with the Recycler view*/
+
+        recyclerView.setAdapter(adapter);
 
     }
 
@@ -97,5 +155,21 @@ public class TeamActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(this, Teams.class));
                 break;
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
